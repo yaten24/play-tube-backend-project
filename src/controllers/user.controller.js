36cd96c0701b5e2ac850fa23c1e675e8960bf069
Yatendra Singh <yaten2404@gmail.com>
2025-03-20 21:28:36 +0530

    added logoutUser method to handle user logout and clear refresh token

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -132,7 +132,34 @@ const loginUser = asyncHandler(async (req, res) => {
     )
 })
 
+//method for logout 
+const logoutUser = asyncHandler(async(req, res) => {
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: undefined
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .clearCookie("", options)
+    .clearCookie("", options)
+    .json(new ApiResponse(200, {}, "User logged out successfully"))
+})
+
 export {
     registerUser,
-    loginUser
+    loginUser,
+    logoutUser
 }
